37a8eec6b1e1ac06a01ba669cdbf46948a4feb3a
Fabio Almeida Lima <fabio.allima@hotmail.com>
2025-07-15 15:21:45 -0300

    first mutation

--- data/db.js
+++ data/db.js
@@ -1,11 +1,16 @@
+let id = 1
+function proximoId() {
+    return id++
+}
+
 const usuarios = [
-    { id: 1, nome: 'João', email: 'joao@teste.com', idade: 30, perfil_id: 1, status: 'ATIVO' },
-    { id: 2, nome: 'Maria', email: 'maria@teste.com', idade: 25, perfil_id: 2, status: 'INATIVO' },
-    { id: 3, nome: 'José', email: 'jose@teste.com', idade: 40, perfil_id: 1, status: 'BLOQUEADO' },]
+    { id: proximoId(), nome: 'João', email: 'joao@teste.com', idade: 30, perfil_id: 1, status: 'ATIVO' },
+    { id: proximoId(), nome: 'Maria', email: 'maria@teste.com', idade: 25, perfil_id: 2, status: 'INATIVO' },
+    { id: proximoId(), nome: 'José', email: 'jose@teste.com', idade: 40, perfil_id: 1, status: 'BLOQUEADO' },]
 
 const perfis = [
     {id: 1, nome: 'Comum'},
     {id: 2, nome: 'Administrador'}
 ]
 
-module.exports = { usuarios, perfis }
+module.exports = { usuarios, perfis, proximoId }
