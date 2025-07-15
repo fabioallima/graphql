34f42dc4e0e23778a5a0abb0fb3b16f4cb806711
Fabio Almeida Lima <fabio.allima@hotmail.com>
2025-07-14 22:29:06 -0300

    feat: add enum

--- data/db.js
+++ data/db.js
@@ -1,7 +1,7 @@
 const usuarios = [
-    { id: 1, nome: 'João', email: 'joao@teste.com', idade: 30, perfil_id: 1},
-    { id: 2, nome: 'Maria', email: 'maria@teste.com', idade: 25, perfil_id: 2},
-    { id: 3, nome: 'José', email: 'jose@teste.com', idade: 40, perfil_id: 1}]
+    { id: 1, nome: 'João', email: 'joao@teste.com', idade: 30, perfil_id: 1, status: 'ATIVO' },
+    { id: 2, nome: 'Maria', email: 'maria@teste.com', idade: 25, perfil_id: 2, status: 'INATIVO' },
+    { id: 3, nome: 'José', email: 'jose@teste.com', idade: 40, perfil_id: 1, status: 'BLOQUEADO' },]
 
 const perfis = [
     {id: 1, nome: 'Comum'},
