503678030c42e29c06b05976d5f393609c3455be
Fabio Almeida Lima <fabio.allima@hotmail.com>
2025-07-14 21:13:51 -0300

    first commit

---
+++ data/db.js
@@ -0,0 +1,11 @@
+const usuarios = [
+    { id: 1, nome: 'João', email: 'joao@teste.com', idade: 30, perfil_id: 1},
+    { id: 2, nome: 'Maria', email: 'maria@teste.com', idade: 25, perfil_id: 2},
+    { id: 3, nome: 'José', email: 'jose@teste.com', idade: 40, perfil_id: 1}]
+
+const perfis = [
+    {id: 1, nome: 'Comum'},
+    {id: 2, nome: 'Administrador'}
+]
+
+module.exports = { usuarios, perfis }
